32f5abc72bd5440065c5fb87f5fb1d226dffe6ae
moraram <moraram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-15 01:26:59 +0000

    Reorganized print functions.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmgs@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/Utilities/IO/Print.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "../Vector.h"
+#include "../TriangularMatrix.h"
+#include "../WeakMatrix.h"
+#include "BasicPrint.h"
+
+namespace Utilities
+{
+	namespace IO
+	{
+		// Declaration
+		template <class TYPE> void Print(const char * format, const Vector<TYPE> & values);
+		template <class TYPE> void Print(const char * format, const Vector<Vector<TYPE> > & values);
+		template <class TYPE> void Print(const char * format, const WeakMatrix<TYPE> matrix);
+		template <class TYPE> void Print(const char * format, const TriangularMatrix<TYPE> & matrix);
+
+		// Definition
+		template <class TYPE>
+		void Print(const char * format, const Vector<TYPE> & values)
+		{
+			for (int j = 0; j < values.Size(); ++j)
+				Print(format, values(j));
+		}
+
+		template <class TYPE>
+		void Print(const char * format, const Vector<Vector<TYPE> > & values)
+		{
+			for (int j = 0; j < values.Size(); ++j)
+				Print(format, values(j));
+		}
+
+		template <class TYPE>
+		void Print(const char * format, const WeakMatrix<TYPE> matrix)
+		{
+			for (int i = 0; i < matrix.NumberOfRows(); ++i)
+			{
+				const int m = matrix.NumberOfColumns() - 1;
+				for (int j = 0; j < m; ++j)
+				{
+					Print(format, matrix(i, j));
+					Print(" ");
+				}
+				Print(format, matrix(i, i));
+				Print("\n");
+			}
+		}
+
+		template <class TYPE>
+		void Print(const char * format, const TriangularMatrix<TYPE> & matrix)
+		{
+			for (int i = 0; i < matrix.NumberOfRows(); ++i)
+			{
+				for (int j = 0; j < i; ++j)
+				{
+					Print(format, matrix(i, j));
+					Print(" ");
+				}
+				Print(format, matrix(i, i));
+				Print("\n");
+			}
+		}
+	}
+}
---
+++ pkg/src/Utilities/IO/Stream.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+
+namespace Utilities
+{
+	namespace IO
+	{
+		extern FILE * stream;
+	}
+}
