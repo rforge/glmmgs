7b0a1ef81d228e4d96c143e4f42aa99331643eb7
moraram <moraram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-23 23:15:22 +0000

    Set default number of iterations equal to 200. Updated manual pages.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmgs@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glmmGS.R
+++ pkg/R/glmmGS.R
@@ -1,5 +1,5 @@
 # Construct list of controls
-glmmGS.Control = function(reltol = 1.e-6, abstol = 1.e-6, maxit = 500)
+glmmGS.Control = function(reltol = 1.e-6, abstol = 1.e-6, maxit = 200)
 {
 	x = list(reltol = reltol, abstol = abstol, maxit = maxit);
 	return(x);
