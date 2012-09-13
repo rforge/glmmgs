aa8d5517f2501ce1348813801c4c35e8c834a09e
moraram <moraram@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-13 09:32:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmgs@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/GlmmGS/RandomEffects/Stratified/CovarianceModels/Functions.h
@@ -0,0 +1,34 @@
+#ifndef GLMMGS_RANDOMEFFECTS_STRATIFIED_COVARIANCEMODELS_FUNCTIONS_H
+#define GLMMGS_RANDOMEFFECTS_STRATIFIED_COVARIANCEMODELS_FUNCTIONS_H
+
+#include "../../../Standard.h"
+
+namespace GlmmGS
+{
+	namespace RandomEffects
+	{
+		namespace Stratified
+		{
+			namespace CovarianceModels
+			{
+				// Weighted square norm of vector
+				double Square(Matrix<const double> m, const Vector<double> & x);
+				double Square(const LDL::SparseMatrix<double> & R, const Vector<double> & x);
+
+				// Trace of diagonal blocks
+				double BlockTrace(int row, int nlevels, const Matrix<double> & a);
+
+				// Trace of diagonal blocks product
+				double BlockSquareTrace(int row, int col, int nlevels, const Matrix<double> & a);
+
+				// Matrix product
+				double MatrixProduct(int k, Matrix<const double> m, const Vector<double> & x);
+
+				// Transposed-matrix product
+				double TMatrixProduct(int k, const LDL::SparseMatrix<double> & R, const Vector<double> & x);
+			}
+		}
+	}
+}
+
+#endif
